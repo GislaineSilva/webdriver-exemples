65b857c376d3f8d16efe59e253980c8a7111224b
Gislaine Silva <gislaine.silva2405@gmail.com>
2018-09-20 23:02:48 +0100

    initial project structure

---
+++ SeleniumTest/GoogleTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using SeleniumTest.POM;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class GoogleTests
+    {
+        [TestMethod]
+        public void TestSearchPluralSight()
+        {
+            using (var driver = new ChromeDriver(@"C:\libraries\"))
+            {
+                var page = new GooglePage(driver);
+
+                page.OpenPage();
+                page.FillSearchBox("Pluralsight");
+                var resultPage = page.ClickSearchButton();
+
+                Assert.AreEqual(
+                    resultPage.FirstLink.Text,
+                    "Pluralsight.com | Pluralsight Online Tutor | Save $100 Now",
+                    "The text on the first result was different of the one expected");
+            }
+        }
+
+        [TestMethod]
+        public void TestSearchMicrosoft()
+        {
+            using (var driver = new ChromeDriver(@"C:\libraries\"))
+            {
+                var page = new GooglePage(driver);
+
+                page.OpenPage();
+                page.FillSearchBox("microsoft");
+                var resultPage = page.ClickSearchButton();
+
+                Assert.IsTrue(
+                    resultPage.FirstLink.Text.ToLower().Contains("microsoft"),
+                    "The text on the first result was different of the one expected");
+            }
+        }
+    }
+}
---
+++ WebDriverDemo/GooglePage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace WebDriverDemo
+{
+    public class GooglePage
+    {
+        private IWebDriver _driver;
+
+        public GooglePage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        [FindsBy(How = How.Name, Using ="q")]
+        public IWebElement SearchBox;
+
+        [FindsBy(How = How.Name, Using = "btnK")]
+        public IWebElement SearchButton;
+
+        public void OpenPage()
+        {
+            _driver.Url = "http://www.google.com";
+
+        }
+        public void FillSearchBox(string text)
+        {
+            SearchBox.SendKeys(text);
+        }
+
+        public bool IsLoaded()
+        {
+            return SearchBox != null && SearchButton != null;
+        }
+
+        public void ClickSearchButton()
+        {
+            SearchButton.Submit();
+        }
+    }
+}
